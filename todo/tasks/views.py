cea4c72795ca7a171fd8774c2bb1ff85c37bb999
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-17 16:45:14 +0500

    day2 -edit-detate-add

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.complete = 'complete' in request.POST  # checkbox orqali
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/update_task.html', {'task': task})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/delete_task.html', {'task': task})
